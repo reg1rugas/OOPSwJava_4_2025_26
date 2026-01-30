3293ad973b7e85128b0d3e277cab5d1e82a229de
Aditya gaur <75514601+reg1rugas@users.noreply.github.com>
2026-01-30 18:53:28 +0530

    Create first.java

---
+++ Experiment 1/first.java
@@ -0,0 +1,28 @@
+class Student {
+    int rollNumber;
+    String name;
+
+    Student() {
+        rollNumber = -1;
+        name = "Not assigned";
+    }
+
+    Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student(7, "Raman");
+        s1.display();
+        s2.display();
+    }
+}
